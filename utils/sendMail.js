4f900deee68fa8d443fc5e6c728afc8d4b1a5bf6
abuhasanmahmud <mahmud.me420@gmail.com>
2023-08-22 12:09:49 +0600

    Feat(V-1) forget password controlar

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,19 +1,23 @@
 
-import { createTransport } from "nodemailer";
+import nodemailer from "nodemailer";
 
 
 export const sendEmail=async(to,subject,text)=>{
-          const transporter=createTransport({
-                    host: process.env.SMPT_HOST,
-                    port: process.env.SMPT_PORT,
-                    auth: {
-                      user: process.env.SMPT_USER,
-                      pass: process.env.SMPT_PASSWORD
-                    }
-                  });
+
+          let config={
+            service:process.env.SMPT_SERVICE,
+            auth:{
+              user:process.env.SMPT_USER,
+              pass:process.env.SMPT_PASS
+            }
+          }
+
+          let transporter=nodemailer.createTransport(config)
 
           await transporter.sendMail({
-                    to,subject,text,
+                    from:process.env.SMPT_USER,
+                    to:to,
+                    text:text
           })
 }
 
